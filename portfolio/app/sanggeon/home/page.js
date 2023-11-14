e0600e1645131a2c2234c9fc1c8f8ee67b178f34
SnagGeon <yangsk0526@gmail.com>
2023-11-14 23:22:13 +0900

    1.배포 테스트 2.홈 수정

--- portfolio/app/sanggeon/home/page.js
+++ portfolio/app/sanggeon/home/page.js
@@ -1,3 +1,15 @@
 export default function page() {
-  return <div>home</div>;
+  return (
+    <div>
+      <div className="move-text-wrap">
+        <p>uiux</p>
+        <img src="/img_dia.svg" alt="" />
+        <p>Web standards</p>
+        <img src="/img_dia.svg" alt="" />
+        <p>DESIGN</p>
+        <img src="/img_dia.svg" alt="" />
+        <p>development</p>
+      </div>
+    </div>
+  );
 }
